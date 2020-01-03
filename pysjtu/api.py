6786423b3c52e86216792df8a128440119efc19c
PhotonQuantum <cy.n01@outlook.com>
2020-01-03 21:02:39 +0800

    first commit

---
+++ pysjtu/api.py
@@ -0,0 +1,36 @@
+import re
+import requests
+import functools
+from . import model
+from . import const
+
+
+class Session:
+    def __init__(self):
+        self._sess = requests.session()
+
+    @property
+    @functools.lru_cache
+    def student_id(self):
+        print("getting id")
+        rtn = self._sess.get(const.HOME_URL)
+        return re.findall(r"(?<=id=\"sessionUserKey\" value=\")\d*", rtn.text)[0]
+
+    @property
+    def cookies(self):
+        return self._sess.cookies
+
+    @cookies.setter
+    def cookies(self, new_cookie):
+        Session.student_id.fget.cache_clear()
+        self._sess.cookies = new_cookie
+
+    def schedule(self, year, term):
+        raw = self._sess.post(const.SCHEDULE_URL, data={"xnm": year, "xqm": const.TERMS[term]})
+        schedule = model.Schedule()
+        schedule.load(raw.json()["kbList"])
+        return schedule
+
+    def _elect(self, params):
+        r = self._sess.post(const.ELECT_URL + self.student_id, data=params)
+        return r.json()
---
+++ pysjtu/const.py
@@ -0,0 +1,4 @@
+HOME_URL = "https://i.sjtu.edu.cn/xtgl/index_initMenu.html"
+SCHEDULE_URL = "https://i.sjtu.edu.cn/kbcx/xskbcx_cxXsKb.html?gnmkdm=N2151"
+ELECT_URL = "https://i.sjtu.edu.cn/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su="
+TERMS = [3, 12, 16]
