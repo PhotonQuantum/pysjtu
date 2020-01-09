af480ade02b2f2e4ddf2ac80c77df1250ca86faf
PhotonQuantum <cy.n01@outlook.com>
2020-01-10 00:40:20 +0800

    feat: score query

--- pysjtu/const.py
+++ pysjtu/const.py
@@ -4,6 +4,8 @@ ELECT_URL = "https://i.sjtu.edu.cn/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N2535
 LOGIN_URL = "https://i.sjtu.edu.cn/jaccountlogin"
 LOGIN_POST_URL = "https://jaccount.sjtu.edu.cn/jaccount/ulogin"
 CAPTCHA_URL = "https://jaccount.sjtu.edu.cn/jaccount/captcha"
+SCORE_URL = "https://i.sjtu.edu.cn/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005"
+SCORE_DETAIL_URL = "https://i.sjtu.edu.cn/cjcx/cjcx_cxXsXmcjList.html?gnmkdm=N305007&su="
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0",
            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"}
 TERMS = [3, 12, 16]
--- pysjtu/model.pyi
+++ pysjtu/model.pyi
@@ -1,4 +1,28 @@
 from typing import List
+
+class ScoreFactor:
+    name: str
+    percentage: float
+    score: float
+
+class Score:
+    name: str
+    teacher: str
+    score: int
+    credit: float
+    gp: float
+    invalid: bool
+    course_type: str
+    category: str
+    score_type: str
+    method: str
+    course_id: str
+    class_name: str
+    class_id: str
+    detail: List[ScoreFactor]
+    year: int
+    term: int
+
 class ScheduleCourse:
     name: str = None
     day: int = None
@@ -11,6 +35,15 @@ class ScheduleCourse:
     teacher: list = None
     meta: list = None
     class_hour: list = None
+class Scores:
+    year: int
+    term: int
+    def load(self, data: dict):
+        pass
+    def all(self) -> List[Score]:
+        pass
+    def filter(self, **param) -> List[Score]:
+        pass
 
 class Schedule:
     def load(self, data: dict):
