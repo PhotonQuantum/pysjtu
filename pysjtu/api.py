9120aee41c1b2425eebace67860931c2eb1f3efd
PhotonQuantum <cy.n01@outlook.com>
2020-01-17 01:03:39 +0800

    refactor: migrate from requests to httpx (breaks existing cookie files)

--- pysjtu/api.py
+++ pysjtu/api.py
@@ -6,7 +6,8 @@ from json.decoder import JSONDecodeError
 from typing import List
 from urllib.parse import urlparse, parse_qs
 
-import requests
+from http.cookiejar import LWPCookieJar
+import httpx
 
 from . import const
 from . import model
@@ -23,31 +24,32 @@ class ServiceUnavailable(Exception):
 
 
 class Session:
-    _sess: requests.Session
+    _client: httpx.Client
     _retry = [.5] * 5 + list(range(1, 5))
 
     def _secure_req(self, ref):
         try:
             return ref()
-        except requests.exceptions.ConnectionError as e:
+        except httpx.exceptions.NetworkError as e:
             req = e.request
-            if req.url[:7] == "http://":
-                req.url = req.url.replace("http://", "https://")
+            if not req.url.is_ssl:
+                req.url = req.url.copy_with(scheme="https", port=None)
             else:
                 raise e
-            return self._sess.send(req)
+            return self._client.send(req)
 
     @staticmethod
     def _http_error_handler(req, *args, **kwargs):
         try:
             req.raise_for_status()
-        except requests.exceptions.HTTPError:
-            if req.status_code == 503:
+        except httpx.exceptions.HTTPError:
+            if req.status_code == httpx.codes.SERVICE_UNAVAILABLE:
                 raise ServiceUnavailable
 
     def __init__(self, retry=None):
-        self._sess = requests.session()
-        self._sess.hooks = {"response": Session._http_error_handler}
+        self._client = httpx.Client()
+        self._client.cookies.jar = LWPCookieJar()
+        self._client.hooks = {"response": Session._http_error_handler}
         self._student_id = None
         self._term_start = None
         if retry: self._retry = retry
@@ -55,71 +57,71 @@ class Session:
     def login(self, username, password):
         self._student_id = None
         for i in self._retry:
-            login_page_req = self._secure_req(partial(self._sess.get, const.LOGIN_URL))
+            login_page_req = self._secure_req(partial(self._client.get, const.LOGIN_URL))
             uuid = re.findall(r"(?<=uuid\": ').*(?=')", login_page_req.text)[0]
-            login_params = parse_qs(urlparse(login_page_req.url).query)
-            login_params = dict(map(lambda x: (x[0], x[1][0]), login_params.items()))
+            login_params = parse_qs(urlparse(str(login_page_req.url)).query)
+            login_params = {k: v[0] for k, v in login_params.items()}
 
-            captcha_img = self._sess.get(const.CAPTCHA_URL, params={"uuid": uuid, "t": int(time.time() * 1000)}).content
+            captcha_img = self._client.get(const.CAPTCHA_URL, params={"uuid": uuid, "t": int(time.time() * 1000)}).content
             captcha = util.recognize_captcha(captcha_img)
 
             login_params.update({"v": "", "uuid": uuid, "user": username, "pass": password, "captcha": captcha})
             result = self._secure_req(
-                partial(self._sess.post, const.LOGIN_POST_URL, params=login_params, headers=const.HEADERS))
-            if "err=1" not in result.url: return
+                partial(self._client.post, const.LOGIN_POST_URL, params=login_params, headers=const.HEADERS))
+            if "err=1" not in result.url.query: return
 
             time.sleep(i)
 
     def load(self, fn):
-        with open(fn, mode="rb") as f:
-            self.cookies = pickle.load(f)
+        new_cookie = LWPCookieJar(fn)
+        new_cookie.load(ignore_discard=True)
+        self.cookies = new_cookie
 
     def dump(self, fn):
-        with open(fn, mode="wb") as f:
-            pickle.dump(self._sess.cookies, f)
+        self._client.cookies.jar.save(fn, ignore_discard=True)
 
     @property
     def proxies(self):
-        return self._sess.proxies
+        return self._client.proxies
 
     @proxies.setter
     def proxies(self, new_proxy: list):
-        self._sess.proxies = new_proxy
+        self._client.proxies = new_proxy
 
     @property
     def cookies(self):
-        return self._sess.cookies
+        return self._client.cookies
 
     @cookies.setter
     def cookies(self, new_cookie):
-        bak_cookie = self._sess.cookies
+        bak_cookie = self._client.cookies
         self._student_id = None
-        self._sess.cookies = new_cookie
+        self._client.cookies = new_cookie
         try:
-            self._secure_req(partial(self._sess.get, const.LOGIN_URL, timeout=5))  # refresh JSESSION token
-        except requests.ReadTimeout:
-            self._sess.cookies = bak_cookie
+            self._secure_req(partial(self._client.get, const.LOGIN_URL, timeout=5))  # refresh JSESSION token
+        except httpx.ReadTimeout:
+            self._client.cookies = bak_cookie
             raise SessionException
-        if "login" in self._sess.get(const.HOME_URL).url:
-            self._sess.cookies = bak_cookie
+        if "login" in self._client.get(const.HOME_URL).url.path:
+            self._client.cookies = bak_cookie
             raise SessionException
 
     @property
     def term_start_date(self):
         if not self._term_start:
-            raw = self._sess.get(const.CALENDAR_URL + self.student_id)
+            raw = self._client.get(const.CALENDAR_URL + self.student_id)
             self._term_start = min(re.findall(r"\d{4}-\d{2}-\d{2}", raw.text))
         return self._term_start
 
     @property
     def student_id(self):
         if not self._student_id:
-            rtn = self._sess.get(const.HOME_URL)
+            rtn = self._client.get(const.HOME_URL)
             self._student_id = re.findall(r"(?<=id=\"sessionUserKey\" value=\")\d*", rtn.text)[0]
         return self._student_id
 
     def schedule(self, year, term) -> model.Schedule:
-        raw = self._sess.post(const.SCHEDULE_URL, data={"xnm": year, "xqm": const.TERMS[term]})
+        raw = self._client.post(const.SCHEDULE_URL, data={"xnm": year, "xqm": const.TERMS[term]})
         schedule = model.Schedule(year, term)
         try:
             schedule.load(raw.json()["kbList"])
@@ -128,8 +130,8 @@ class Session:
         return schedule
 
     def _get_score_detail(self, year, term, class_id) -> List[model.ScoreFactor]:
-        raw = self._sess.post(const.SCORE_DETAIL_URL + self.student_id,
-                              data={"xnm": year, "xqm": const.TERMS[term], "jxb_id": class_id, "_search": False,
+        raw = self._client.post(const.SCORE_DETAIL_URL + self.student_id,
+                                data={"xnm": year, "xqm": const.TERMS[term], "jxb_id": class_id, "_search": False,
                                     "nd": int(time.time() * 1000), "queryModel.showCount": 15,
                                     "queryModel.currentPage": 1, "queryModel.sortName": "",
                                     "queryModel.sortOrder": "asc", "time": 1})
@@ -137,7 +139,7 @@ class Session:
         return factors
 
     def score(self, year, term) -> model.Scores:
-        raw = self._sess.post(const.SCORE_URL, data={"xnm": year, "xqm": const.TERMS[term], "_search": False,
+        raw = self._client.post(const.SCORE_URL, data={"xnm": year, "xqm": const.TERMS[term], "_search": False,
                                                      "nd": int(time.time() * 1000), "queryModel.showCount": 15,
                                                      "queryModel.currentPage": 1, "queryModel.sortName": "",
                                                      "queryModel.sortOrder": "asc", "time": 1})
@@ -149,8 +151,8 @@ class Session:
         return scores
 
     def exam(self, year, term) -> model.Exams:
-        raw = self._sess.post(const.EXAM_URL,
-                              data={"xnm": year, "xqm": const.TERMS[term], "_search": False, "ksmcdmb_id": None,
+        raw = self._client.post(const.EXAM_URL,
+                                data={"xnm": year, "xqm": const.TERMS[term], "_search": False, "ksmcdmb_id": None,
                                     "kch": None, "kc": None, "ksrq": None, "kkbm_id": None,
                                     "nd": int(time.time() * 1000), "queryModel.showCount": 15,
                                     "queryModel.currentPage": 1, "queryModel.sortName": "",
@@ -177,10 +179,10 @@ class Session:
             if k in dir():
                 req_params[v] = locals()[k]
 
-        req = partial(self._sess.post, const.COURSELIB_URL + self.student_id)
+        req = partial(self._client.post, const.COURSELIB_URL + self.student_id)
 
         return model.QueryResult(req, partial(util.schema_post_loader, schema.LibCourseSchema), req_params)
 
     def _elect(self, params):
-        r = self._sess.post(const.ELECT_URL + self.student_id, data=params)
+        r = self._client.post(const.ELECT_URL + self.student_id, data=params)
         return r.json()
