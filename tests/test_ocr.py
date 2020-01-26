04e555f6a77bed694087c97ff778187ac43ff6da
PhotonQuantum <cy.n01@outlook.com>
2020-01-26 23:47:51 +0800

    test(ocr): move test resources into new directory

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -4,9 +4,9 @@ import pytest
 
 from pysjtu.ocr import SVMRecognizer, NNRecognizer
 
-DATA_DIR = path.join(path.dirname(path.abspath(__file__)), 'data')
-MODEL_FILE = path.join(path.dirname(path.abspath(".")), 'model.pickle')
-NN_MODEL_FILE = path.join(path.dirname(path.abspath(".")), 'ckpt.pth')
+CAPTCHA_DIR = path.join(path.dirname(path.abspath(__file__)), 'resources/captcha')
+MODEL_FILE = path.join(path.abspath("."), 'model.pickle')
+NN_MODEL_FILE = path.join(path.abspath("."), 'ckpt.pth')
 
 
 @pytest.mark.xfail(reason="SVM-based and ResNet-based recognizer doesn't recognize captcha correctly sometime.")
@@ -15,7 +15,7 @@ NN_MODEL_FILE = path.join(path.dirname(path.abspath(".")), 'ckpt.pth')
     "mwgi", "qkxvx", "qrdxb", "rysv", "zwtco"
 ])
 def test_svm_recognizer(predictor, captcha):
-    assert predictor.recognize(open(path.join(DATA_DIR, captcha + ".jpg"), mode="rb").read()) == captcha
+    assert predictor.recognize(open(path.join(CAPTCHA_DIR, captcha + ".jpg"), mode="rb").read()) == captcha
 
 
 @pytest.mark.cuda
@@ -25,4 +25,4 @@ def test_svm_recognizer(predictor, captcha):
 ])
 def test_resnet_recognizer_cuda(captcha):
     predictor = NNRecognizer(NN_MODEL_FILE, use_cuda=True)
-    assert predictor.recognize(open(path.join(DATA_DIR, captcha + ".jpg"), mode="rb").read()) == captcha
+    assert predictor.recognize(open(path.join(CAPTCHA_DIR, captcha + ".jpg"), mode="rb").read()) == captcha
