9fd39036aecffdbdf2535d8d3ddeedf1b97a12a9
PhotonQuantum <self@lightquantum.me>
2020-07-18 03:45:27 +0800

    feat(selection): query available sectors

--- pysjtu/consts.py
+++ pysjtu/consts.py
@@ -5,7 +5,6 @@ DEFAULT_BASE_URL = "https://i.sjtu.edu.cn"
 
 HOME_URL = "/xtgl/index_initMenu.html"
 SCHEDULE_URL = "/kbcx/xskbcx_cxXsKb.html?gnmkdm=N2151"
-ELECT_URL = "/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su="
 LOGIN_URL = "/jaccountlogin"
 LOGOUT_URL = "/logout"
 SCORE_URL = "/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N305005"
@@ -17,6 +16,8 @@ COURSELIB_URL = "/design/funcData_cxFuncDataList.html?func_widget_guid=DA1B5BB30
 GPA_PARAMS_URL = "/cjgl/common_cxGnzdxxList.html?gnmkdm=N309130"
 GPA_CALC_URL = "/cjpmtj/gpapmtj_tjGpapmtj.html?gnmkdm=N309131&su="
 GPA_QUERY_URL = "/cjpmtj/gpapmtj_cxGpaxjfcxIndex.html?doType=query&gnmkdm=N309131&su="
+SELECTION_ALL_SECTORS_PARAM_URL = "/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su="
+SELECTION_SECTOR_PARAM_URL = "/xsxk/zzxkyzb_cxZzxkYzbDisplay.html?gnmkdm=N253512&su="
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0",
            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"}
