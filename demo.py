913a7fb3c5119fd22d21e6e6dd956556b26af2a0
PhotonQuantum <self@lightquantum.me>
2020-08-13 05:28:10 +0800

    chore: add async demo

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 import os
 
-from pysjtu import Session, Client, CourseRange, NNRecognizer
+from pysjtu import AsyncSession, Client, CourseRange, NNRecognizer
+from pysjtu.exceptions import GPACalculationException
+import asyncio
 
 # from datetime import datetime, timezone
 # import arrow
@@ -21,57 +23,63 @@ lesson_time = (((8, 0), (8, 45)),
                ((19, 55), (20, 40)),
                ((20, 55), (21, 40)))
 
-try:
-    sess_file = open("session", mode="r+b")
-except FileNotFoundError:
-    sess_file = None    # type: ignore
-sess = Session(ocr=NNRecognizer())
+async def main():
+    try:
+        sess_file = open("session", mode="r+b")
+    except FileNotFoundError:
+        sess_file = None    # type: ignore
 
-with Session(session_file=sess_file) if sess_file else Session(username=os.environ["SJTU_USER"],
-                                                               password=os.environ["SJTU_PASS"]) as sess:
-    client = Client(session=sess)
+    async with AsyncSession(session_file=sess_file) if sess_file else AsyncSession(username=os.environ["SJTU_USER"],
+                                                                   password=os.environ["SJTU_PASS"]) as sess:
+        client = Client(session=sess)
 
-    print(client.student_id)
-    schedule = client.schedule(2019, 1)
-    print(schedule)
-    print(schedule.filter(name="高等数学II"))
-    print(list(map(lambda x: x.name, schedule.filter(week=2, day=range(1, 3), time=[range(10, 12), 1]))))
+        print(await client.student_id)
+        schedule = await client.schedule(2019, 1)
+        print(schedule)
+        print(schedule.filter(name="高等数学II"))
+        print(list(map(lambda x: x.name, schedule.filter(week=2, day=range(1, 3), time=[range(10, 12), 1]))))
 
-    '''
-    c = Calendar()
-    local_tz = datetime.now(timezone.utc).astimezone().tzinfo
-    for lesson in schedule:
-        e = Event()
-        e.name = lesson.name
-        e.begin = arrow.get(datetime(2020, 1, 5 + lesson.day, *lesson_time[lesson.time[0] - 1][0], tzinfo=local_tz))
-        e.end = arrow.get(datetime(2020, 1, 5 + lesson.day, *lesson_time[lesson.time[-1] - 1][1], tzinfo=local_tz))
-        e.location = lesson.location
-        c.events.add(e)
+        '''
+        c = Calendar()
+        local_tz = datetime.now(timezone.utc).astimezone().tzinfo
+        for lesson in schedule:
+            e = Event()
+            e.name = lesson.name
+            e.begin = arrow.get(datetime(2020, 1, 5 + lesson.day, *lesson_time[lesson.time[0] - 1][0], tzinfo=local_tz))
+            e.end = arrow.get(datetime(2020, 1, 5 + lesson.day, *lesson_time[lesson.time[-1] - 1][1], tzinfo=local_tz))
+            e.location = lesson.location
+            c.events.add(e)
 
-    with open("test.ics", mode="w") as f:
-        f.write(str(c))
-    '''
+        with open("test.ics", mode="w") as f:
+            f.write(str(c))
+        '''
 
-    score = client.score(2019, 0)
-    print(score)
-    print(score[0].detail)
+        score = await client.score(2019, 0)
+        print(score)
+        print(await score[0].detail)
 
-    exam = client.exam(2019, 0)
-    print(exam)
+        exam = await client.exam(2019, 0)
+        print(exam)
 
-    query = client.query_courses(2019, 0, name="高等数学")
-    print(query[3:18])
-    for item in query:
-        print(item, end=" ")
-    print()
+        query = await client.query_courses(2019, 0, name="高等数学")
+        print(await query[3:18])
+        async for item in query:
+            print(item, end=" ")
+        print()
 
-    print(client.term_start_date)
-    query_params = client.default_gpa_query_params
+        print(await client.term_start_date)
+        query_params = await client.default_gpa_query_params
 
-    print(client.gpa(query_params, 10))
+        try:
+            print(await client.gpa(query_params))
 
-    query_params.course_range = CourseRange.ALL
-    print(client.gpa(query_params, 10))
+            query_params.course_range = CourseRange.ALL
+            print(await client.gpa(query_params))
+        except GPACalculationException:
+            print("gpa can't be read.")
 
-    if not sess_file:
-        sess.dump(open("session", mode="wb"))
+        if not sess_file:
+            sess.dump(open("session", mode="wb"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
