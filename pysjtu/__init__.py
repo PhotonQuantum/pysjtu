8a0535505fe786634f57a2c4c716eb74e7a77ff2
LightQuantum <self@lightquantum.me>
2022-06-21 03:47:01 +0800

    chore: migrate to poetry
    chore: fix ci
    
    fix: doc build error
    
    fix: docs dependency
    
    chore(CI): simplify caching
    
    fix(CI): quote python version
    
    fix(CI): missing flake8 dependency
    
    fix(CI): ignore venv in flake8

--- pysjtu/__init__.py
+++ pysjtu/__init__.py
@@ -1,6 +1,8 @@
+import importlib.metadata
+
 from pysjtu.ocr import LegacyRecognizer, NNRecognizer, JCSSRecognizer
 from .client import Client, create_client
 from .models import CourseRange, LogicEnum, Ranking
 from .session import Session
 
-__version__ = "0.3.5"
+__version__ = importlib.metadata.version("pysjtu")  # broken in local env due to bug in poetry 1.1
